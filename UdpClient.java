bd0522b5ddbf5d9502a05e0e6a76926d38ea2866
nimamasoumi <mraghahe@protonmail.com>
2023-11-03 17:57:11 -0400

    UDP client-server app

---
+++ UdpClient.java
@@ -0,0 +1,10 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+class UdpClient
+{
+    public static void main(String[] args)
+    {
+        
+    }
+}
---
+++ UdpServer.java
@@ -0,0 +1,11 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+class UdpServer
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
+    
