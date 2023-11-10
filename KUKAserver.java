924ba3fe3e263af458f5aacc4bb4f3c8d423b2dc
nimamasoumi <mraghahe@protonmail.com>
2023-11-10 17:51:21 -0500

    Including the initialization and closing mechanisms

--- KUKAserver.java
+++ KUKAserver.java
@@ -7,6 +7,7 @@ This class implements the singletons design pattern.
 import java.io.Closeable;
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.InetSocketAddress;
 
 class KUKAserver implements Runnable, Closeable
 {
@@ -29,19 +30,85 @@ class KUKAserver implements Runnable, Closeable
         return instance;
     }
 
+    private ServerSocket serversocket = null;
+    private int listenport = 0;
+    private static final int sockettimeout = 20;
+
+    private synchronized boolean listen()
+    {
+        System.out.println("Initializing the server socket ... ");
+        try
+        {
+            serversocket = new ServerSocket();
+            serversocket.setReuseAddress(true);
+            serversocket.bind(new InetSocketAddress(listenport));
+            serversocket.setSoTimeout(sockettimeout);
+        }
+        catch(Exception e)
+        {
+            System.out.println("problem initializing the server socket."+e);
+            return false;
+        }
+        return true;
+    }
+
     @Override
     public void run()
     {
-
+        listen();
     }
 
     @Override
     public void close() throws IOException
     {
+        System.out.println("Closing the socket ... ");
+        try
+        {
+            if(serversocket!=null)
+            {
+                serversocket.close();
+            }
+        }
+        catch(IOException e)
+        {
+            throw e;
+        }
+        finally
+        {
+            serversocket=null;
+        }
+    }
 
+    public synchronized boolean stop()
+    {
+        try
+        {
+            close();
+        }
+        catch(IOException e)
+        {
+            System.out.println("Problem closing the socket ... "+e);
+            return false;
+        }
+        return true;
+    }
+
+    public void setListenPort(int _listenport)
+    {
+        this.listenport = _listenport;
     }
     public static void main(String[] args)
     {
+        final int port = 30005;
 
+        System.out.println("The server is starting ... ");
+        final var server = new KUKAserver();
+        server.setListenPort(port);
+        var tserver = new Thread(server);
+        tserver.start();
+
+        // Shutting down the server
+        server.stop();
+        System.out.println("The server is shut down ...\n");
     }
 }
