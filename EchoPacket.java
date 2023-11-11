2b9d62beac2a3140813dde17a4af65ea10e45be9
nimamasoumi <mraghahe@protonmail.com>
2023-11-10 21:09:43 -0500

    This packet listener echos the client messages

---
+++ EchoPacket.java
@@ -0,0 +1,13 @@
+// Echo the received packets back to the client
+class EchoPacket implements IPacketListener
+{
+    private KUKAserver server = KUKAserver.getInstance();
+
+    @Override
+    public void handlePacket(Packet _p)
+    {
+        System.out.println("Received packet: "+_p.serialize());
+        // echo the packet
+        server.send(_p);
+    }
+}
