2a948ed6bc3144735c041599942be637fda09174
nimamasoumi <mraghahe@protonmail.com>
2023-11-13 18:17:21 -0500

    Completing the send method

--- KUKAserver.java
+++ KUKAserver.java
@@ -8,6 +8,9 @@ import java.util.List;
 import java.util.ArrayList;
 import java.io.Closeable;
 import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
 import java.net.ServerSocket;
 import java.net.InetSocketAddress;
 
@@ -33,9 +36,32 @@ class KUKAserver implements Runnable, Closeable
     }
 
     private ServerSocket serversocket = null;
+    private Socket socket = null;
     private int listenport = 0;
     private static final int sockettimeout = 20;
     private List<IPacketListener> packetlisteners = new ArrayList<IPacketListener>();
+
+    private static void safeSleep(long _s)
+    {
+        try
+        {
+            Thread.sleep(_s);            
+        }
+        catch(Exception e)
+        {
+
+        }
+    }
+
+    public boolean isConnected()
+    {
+        return serversocket!=null && serversocket.isBound();
+    }
+    
+    public boolean isClientConnected()
+    {
+        return isConnected() && socket != null && socket.isConnected();
+    }
 
     private synchronized boolean listen()
     {
@@ -59,6 +85,21 @@ class KUKAserver implements Runnable, Closeable
     public void run()
     {
         listen();
+        while(serversocket!=null && serversocket.isBound())
+        {
+            if (socket == null || !socket.isConnected() || socket.isClosed())
+            {
+                try
+                {
+                    socket = serversocket.accept();
+                }
+                catch(IOException e)
+                {
+                    safeSleep(1000);
+                    continue;
+                }
+            }
+        }
     }
 
     @Override
@@ -71,6 +112,10 @@ class KUKAserver implements Runnable, Closeable
             {
                 serversocket.close();
             }
+            if(socket!=null)
+            {
+                socket.close();
+            }
         }
         catch(IOException e)
         {
@@ -79,6 +124,7 @@ class KUKAserver implements Runnable, Closeable
         finally
         {
             serversocket=null;
+            socket=null;
         }
     }
 
@@ -108,7 +154,23 @@ class KUKAserver implements Runnable, Closeable
     // send packets to connected clients
     public synchronized void send(Packet _p)
     {
+        if(!isClientConnected() || socket.isClosed())
+        {
+            System.out.println("No client is connected, dropping packet: "+_p.serialize());
+            return;
+        }
 
+        try
+        {
+            var writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            writer.write(_p.serialize()+"\n");
+            writer.flush();
+        }
+        catch(Exception e)
+        {
+            System.out.println("Failed sending packet: "+
+                _p.serialize()+"\n"+e);
+        }
     }
     public static void main(String[] args)
     {
@@ -124,6 +186,8 @@ class KUKAserver implements Runnable, Closeable
         // running the server thread
         var tserver = new Thread(server);
         tserver.start();
+
+        safeSleep(1000);
 
         // Shutting down the server
         server.stop();
