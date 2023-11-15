c1c0ff9e72c607e2bcf33a8c8d9813af046ed193
nimamasoumi <mraghahe@protonmail.com>
2023-11-14 19:21:30 -0500

    Re-organizing the files

--- IPacketListener.java
+++
@@ -1,7 +0,0 @@
-// Packet listener for the KUKAserver
-
-interface IPacketListener
-{    
-    // handlePacket is called when it is recevied from the client
-    public void handlePacket(Packet _p);
-}
--- PacketType.java
+++
@@ -1,6 +0,0 @@
-public enum PacketType
-{
-    Unknown,
-    MessagePacket,
-    DisconnectPacket
-}
