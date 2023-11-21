a800ada8f7d5a98303bf23bcb61f0aea1963ab61
nimamasoumi <mraghahe@protonmail.com>
2023-11-21 18:03:42 -0500

    Completing the IDLE status handler

--- tasks/Task_Idle.java
+++ tasks/Task_Idle.java
@@ -6,11 +6,14 @@ import server.data.KUKAAppStates;
 import packets.StateChangePacket;
 import utils.StateChanger;
 
+/*This singletons class manages the server in the idle status.
+ * It does nothing until there is command from the client.
+ */
 public class Task_Idle implements ITask{
     
     private KUKAserver medServer = KUKAserver.getInstance();
-    private boolean isCancel = false;
-    private IPacketListener listener = null;
+    public static boolean isCancel = false;
+    private static IPacketListener listener = null;
 
     private static volatile Task_Idle instance = null;
 
@@ -33,13 +36,35 @@ public class Task_Idle implements ITask{
     
     private void addPacketHandler()
     {
-        this.listener = new StateChanger();    
+        Task_Idle.listener = new StateChanger();    
+        medServer.addListener(listener);
     }
     
     @Override
     public boolean run()
     {
+        System.out.println("Running the IDLE task");
+        if (!this.initialize())
+        {
+            return false;
+        }
+        // waiting for the commands from the client and doing nothing
+        while(!Task_Idle.isCancel)
+        {
+            if(medServer.isClientDisconnected())
+            {
+                System.out.println("Client Disconnected.");
+                isCancel = true;
+                break;
+            }
 
+            try
+            {
+                Thread.sleep(100);
+            }
+            catch (Exception e){}            
+        }
+        return this.reset();
     }
 
     @Override
@@ -54,6 +79,9 @@ public class Task_Idle implements ITask{
     @Override
     public boolean reset()
     {
-
+        System.out.println("Reseting the IDLE task controller.");
+        medServer.removeListener(listener);
+        isCancel = false;
+        return true;
     }
 }
